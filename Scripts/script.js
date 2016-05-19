2eaa88677bcb42ac565ebefdfe749853541edeb4
Yordi Coussement <yordi.coussement@immalle.eu>
2016-05-19 14:56:24 +0200

    upload naar GH Pages

---
+++ Scripts/script.js
@@ -0,0 +1,98 @@
+var antwerpen = document.getElementById("Anvers");
+antwerpen.addEventListener("click", function() {
+  swal({
+		title: "Anvers",
+		text: "Voici la grande place d’Anvers. L’œuvre d'art que vous voyez est Brabo. C’est une œuvre d’art très connue à Anvers. Chaque jours il y a beaucoup de visiteurs sur cette grande place.",
+		imageUrl: "img/antwerpen.jpg"
+	});
+});
+
+var limburg = document.getElementById("Limbourg");
+limburg.addEventListener("click", function() {
+  swal({
+		title: "Limbourg",
+		text: "Au Limbourg on parle aussi le néerlandais, mais les habitants parlent tous très lentement.",
+		imageUrl: "img/limburg.png"
+	});
+});
+
+var oostv = document.getElementById("FlandreOrientale");
+oostv.addEventListener("click", function() {
+  swal({
+		title: "Flandre Orientale",
+		text: "En Belgique il y a  aussi le football bien sûr. En Flandre Orientale il y a le plus beau stade de foot avec le nom ‘Ghelamco Arena’. C’est le stade de AA Gent.",
+		imageUrl: "img/oostv.jpg"
+	});
+});
+
+var westv = document.getElementById("FlandreOccidentale");
+westv.addEventListener("click", function() {
+  swal({
+		title: "Flandre Occidentale",
+		text: "La Flandre Occidentale est une destination très amusante en Belgique. Il y la belle ville de Bruges, mais aussi la côte. Pendant les vacances d’été, il y a beaucoup d’évènements à la côte. Quelques exemples :  Ostend Beach, Beachland, Q-Beach House,…",
+		imageUrl: "img/westv.jpg"
+	});
+});
+
+var wbra = document.getElementById("Brabantwallon");
+wbra.addEventListener("click", function() {
+  swal({
+		title: "Brabant wallon",
+		text: "En Brabant Wallon, il y ai le parc d'attractions Walibi. C’est le meilleur parc d’attractions en Belgique.",
+		imageUrl: "img/wbra.jpg"
+	});
+});
+
+var brussel = document.getElementById("Bruxelles");
+brussel.addEventListener("click", function() {
+  swal({
+		title: "Bruxelles",
+		text: "Pour commencer… Bruxelles est une province en Belgique. Donc la Belgique n’est pas une province de Bruxelles. Beaucoup de gens pensent que Bruxelles est le pays mais le vrai pays est la Belgique, qui comprend une région francophone (au sud), néerlandophone (au nord) et allemande (près de la frontière allemande). Quelques curiosités populaires sont le Petit Julien (Manneke Pis en néerlandais) et l’Atomium.",
+		imageUrl: "img/brussel.jpg"
+	});
+});
+
+var henegouwen = document.getElementById("Hainaut");
+henegouwen.addEventListener("click", function() {
+  swal({
+		title: "Hainaut",
+		text: "En Hainaut Il y a le plus beau zoo de Belgique. C'est un zoo très grand et les bâtiments sont dans le thème des animaux.",
+		imageUrl: "img/hainaut.jpg"
+	});
+});
+
+var luik = document.getElementById("Liege");
+luik.addEventListener("click", function() {
+  swal({
+		title: "Liège",
+		text: "Liège est connue pour son eau minérale SPA et Chaudfontaine.",
+		imageUrl: "img/luik.png"
+	});
+});
+
+var luxemburg = document.getElementById("Luxembourg");
+luxemburg.addEventListener("click", function() {
+  swal({
+		title: "Luxembourg",
+		text: "Dans la province de Luxembourg on trouve beaucoup de légendes dans chaque village ou ville.",
+		imageUrl: "img/luxemburg.jpg"
+	});
+});
+
+var namen = document.getElementById("Namur");
+namen.addEventListener("click", function() {
+  swal({
+		title: "Namur",
+		text: "Namur est la capitale de Wallonie et le parlement de Wallonie est situé ici.",
+		imageUrl: "img/namen.jpg"
+	});
+});
+
+var vbra = document.getElementById("Brabantflamand");
+vbra.addEventListener("click", function() {
+  swal({
+		title: "Brabant flamand",
+		text: "Une des villes les plus connues est Louvain. Il y a aussi quelques curiosités intéressantes. Ci-dessus vous voyez le Château de Gaasbeek. C’est un château très connu dans cette région.",
+		imageUrl: "img/vbra.jpg"
+	});
+});
